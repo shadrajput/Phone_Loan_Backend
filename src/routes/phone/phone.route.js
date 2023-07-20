80f77940b07199e3a8967c828fe996e22ffd9c8b
shadrajput <shadrajput6@gmail.com>
2023-07-20 19:18:51 +0530

    Company Details

--- src/routes/phone/phone.route.js
+++ src/routes/phone/phone.route.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const {AddModel , searchPhone, getallModel , getSingleModel , updateModelDetails , deleteModelDetails} = require("../../routes/phone/phone.controller")
+const {AddModel , searchPhone, getallModel , getModelByCompany , getSingleModel , updateModelDetails , deleteModelDetails} = require("../../routes/phone/phone.controller")
 
 const router = express.Router();
 
 router.post("/addphone" , AddModel)
 router.get("/search/:model" , searchPhone)
 router.get("/List/:pageNo" , getallModel)
+router.get("/getModelByCompany/:id" , getModelByCompany)
 router.get("/details/:id" , getSingleModel)
 router.put("/update/:id" , updateModelDetails)
 router.delete("/delete/:id" , deleteModelDetails)
